c5780e3736716a1e2863aaf4aecaa9daba022ae7
Jia <49358172+craftslab@users.noreply.github.com>
2022-02-07 15:05:38 +0800

    Update test.sh

--- script/test.sh
+++ script/test.sh
@@ -3,12 +3,12 @@
 go env -w GOPROXY=https://goproxy.cn,direct
 
 if [ "$1" = "report" ]; then
-  GIN_MODE=release go test -cover -covermode=atomic -coverprofile=coverage.txt -parallel 2 -race -v ./...
+  go test -cover -covermode=atomic -coverprofile=coverage.txt -parallel 2 -race -v ./...
 else
   list="$(go list ./... | grep -v foo)"
   old=$IFS IFS=$'\n'
   for item in $list; do
-    GIN_MODE=release go test -cover -covermode=atomic -parallel 2 -race -v "$item"
+    go test -cover -covermode=atomic -parallel 2 -race -v "$item"
   done
   IFS=$old
 fi
