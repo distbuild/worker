20a0f34556129c60e9346e68a87e91ea6bb69df9
Jia Jia <jia.jia@zte.com.cn>
2022-03-08 15:19:51 +0800

    Upgrade rust-version to 1.59

--- src/arg/arg.rs
+++ src/arg/arg.rs
@@ -1,4 +1,4 @@
-use clap::{App, Arg};
+use clap::{Arg, Command};
 use std::error::Error;
 
 #[derive(Clone, Default)]
@@ -12,7 +12,7 @@ impl Argument {
         self.version_info =
             concat!(env!("CARGO_PKG_VERSION"), "-build-", env!("build")).to_string();
 
-        let matches = App::new("worker")
+        let matches = Command::new("worker")
             .version(&*self.version_info)
             .arg(
                 Arg::new("config_file")
